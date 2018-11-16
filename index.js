bfe17b210fe3cb7f809ab9b92ef8dd9272639f16
Anu Khambete <anukhambete@gmail.com>
2018-11-16 02:49:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ function BoardMember(name, homeState, training){
 BoardMember.prototype.veto = function() {
   return "No, I must disagree"
 }
+
+BoardMember.prototype.approve = function() {
+  return "You can do that!"
+}
 //   this.veto = function(){
 //     return 'No, I must disagree'
 //   }
